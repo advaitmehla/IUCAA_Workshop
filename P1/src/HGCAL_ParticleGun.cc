89d402f2db34a4d587e4305e0bfb2ed232d040c6
advaitmehla <77231014+advaitmehla@users.noreply.github.com>
2022-12-06 18:23:35 +0530

    1c done

--- P1/src/HGCAL_ParticleGun.cc
+++ P1/src/HGCAL_ParticleGun.cc
@@ -2,14 +2,14 @@
 #include "G4ParticleTable.hh"
 #include "G4SystemOfUnits.hh"
 #include "G4RandomDirection.hh"
-#include "G4Gamma.hh"
+#include "G4Proton.hh"
 HGCAL_PrimaryGeneratorAction :: HGCAL_PrimaryGeneratorAction()
 {
     int num = 1;
     fParticleGun = new G4ParticleGun(num);
     // G4ParticleTable *ptable = G4ParticleTable::GetParticleTable();
     // G4ParticleDefinition *particle = ptable->FindParticle("proton");
-    G4ParticleDefinition *particle = G4Gamma::Definition();
+    G4ParticleDefinition *particle = G4Proton::Definition();
     fParticleGun->SetParticleDefinition(particle);
     fParticleGun->SetParticleMomentumDirection(G4ThreeVector(0.,0.,-1.));
     fParticleGun->SetParticleEnergy(3.*GeV);
